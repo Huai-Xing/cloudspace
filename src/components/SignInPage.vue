a3afcbc52100cdc18bbfd52349271c0c44465cd9
leannetan <65459434+leannetan@users.noreply.github.com>
2021-03-22 01:07:14 +0800

    Changed styling

--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -1,52 +1,68 @@
 <template>
-<div>
+  <div>
     <ul class="top-nav-list">
-      <li><router-link :to="{ name: 'signin', params: { newUser: false }}"> Log In </router-link></li>
-      <li><router-link :to="{ name: 'signin', params: { newUser: true }}"> Sign Up </router-link></li>
+      <li>
+        <router-link :to="{ name: 'signin', params: { newUser: false } }">
+          Log In
+        </router-link>
+      </li>
+      <li> | </li>
+      <li>
+        <router-link :to="{ name: 'signin', params: { newUser: true } }">
+          Sign Up
+        </router-link>
+      </li>
     </ul>
 
-  <div class="row">
+    <img id="leftphoto" src="../assets/background5_signin.png" />
 
-    <!-- Left Column -->
-    <div class="column">
-      <img id="leftphoto" src="../assets/background5_signin.png">
+    <div id="chunk">
+      <p id="sign" v-show="newUser">Sign Up</p>
+      <p id="sign" v-show="!newUser">Log In</p>
+      <form>
+        <div v-show="newUser">
+          <label for="name">Name:</label><br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="E.g. John Doe"
+            required
+          /><br />
+        </div>
+        <label for="email">Email:</label><br />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="user@domain.com"
+          required
+        /><br />
+
+        <label for="password">Password:</label><br />
+        <input
+          type="text"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+        /><br />
+
+        <input type="checkbox" id="checkbox" />
+        <label for="checkbox" id="cblabel">Keep me logged in</label><br />
+
+        <button type="submit" value="Submit" id="submit" v-on:click="logIn()">
+          Let's go!
+        </button>
+      </form>
     </div>
 
-    <!-- Middle Column -->    
-    <div class="column">
-      <div id="chunk">
-        <p id="sign" v-show="newUser"> Sign Up </p>
-        <p id="sign" v-show="!newUser"> Log In </p>
-        <form>
-          <div v-show="newUser">
-            <label for="name" >Name:</label><br>
-            <input type="text" id="name" name="name" placeholder="E.g. John Doe"  required><br>
-          </div> 
-          <label for="email">Email:</label><br>
-          <input type="text" id="email" name="email" placeholder="user@domain.com" required><br>
+    <img id="rightphoto" src="../assets/background4_signin.png" />
 
-          <label for="password">Password:</label><br>
-          <input type="text" id="password" name="password" placeholder="Enter your password" required><br>
+    <!-- SignUpPage Content -->
 
-          <input type="checkbox" id="checkbox">
-          <label for="checkbox" id="cblabel">Keep me logged in</label><br>
-
-          <button type="submit" value="Submit" id="submit" v-on:click="logIn()" > Let's go! </button>
-    
-        </form>
-      </div>
-    </div>
-
-    <!-- Right Column -->
-    <div class="column">
-      <img id="rightphoto" src="../assets/background4_signin.png">
-    </div>
-
+    <pgFooter></pgFooter>
   </div>
-  <!-- SignUpPage Content -->
-
-  <pgFooter></pgFooter>
-</div>
 </template>
 
 
@@ -54,31 +70,37 @@
 export default {
   data() {
     return {
-      newUser: this.$route.params.newUser
-    }
+      newUser: this.$route.params.newUser,
+    };
   },
   // mtds
   methods: {
     // Fn to push the user to '/login/calendar' once the Let's go button is clicked
-    logIn: function() {
+    logIn: function () {
       // to push user to '/login/calendar'
-      this.$router.push({name: "Calendar"});
+      this.$router.push({ name: "Calendar" });
 
       // Other logic (retriving details from firebase for the specific user?) here
-    }
+    },
   },
   //Register Locally
-  components: {
-  }
-}
+  components: {},
+  beforeRouteUpdate(to, from, next) {
+    this.newUser = to.params.newUser;
+    next();
+  },
+};
 </script>
 
 
 <style scoped>
 * {
-  font-family: Roboto;
   font-weight: lighter;
-  font-size: 13px
+  font-size: 13px;
+}
+body {
+  min-width: 100%;
+  width: 100%;
 }
 img {
   width: 300px;
@@ -92,46 +114,41 @@ ul {
   margin: 0;
   padding: 0;
 }
+
 li {
+  font-family: "Lora";
   display: inline;
   font-size: 14px;
-  margin-left: 10px;
-  margin-right: 10px;
+  margin-left: 5px;
+  margin-right: 5px;
   float: right;
 }
 
-.column {
-  float:left;
-  width: 33.33%;
-  height: 100%;
-}
-
-.row {
-  content: "";
-  display: table;
-  clear: both;
-  width: 100%;
-  height: 100%;
+a {
+  color: black;
 }
 
 #leftphoto {
-  position: relative;
-  top: 90%;
-  left:10%;
-  width: 340px;
+  position: absolute;
+  left: 3%;
+  bottom: 5%;
+  width: 22%;
+  height: auto;
 }
 
 #rightphoto {
-  position: relative;
-  top: 30%;
-  width: 360px;
+  position: absolute;
+  top: 12%;
+  right: 3%;
+  width: 26%;
+  height: auto;
 }
 
 #chunk {
-  position: relative;
+  position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-40%, -40%);
+  transform: translate(-50%, -50%);
 }
 
 #sign {
@@ -185,7 +202,6 @@ input {
   margin-top: 12px;
   font-family: Lora;
   border: none;
-  background-color: #BEDAAE;
+  background-color: #bedaae;
 }
-
 </style>
