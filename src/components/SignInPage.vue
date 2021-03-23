7a5960946a3debc12f452f9a9aee117bcc2b72e7
shuling127 <65823900+shuling127@users.noreply.github.com>
2021-03-23 15:37:45 +0800

    Fixed sign up and sign in without auth bugs

--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -6,7 +6,7 @@
           Log In
         </router-link>
       </li>
-      <li> | </li>
+      <li>|</li>
       <li>
         <router-link :to="{ name: 'signin', params: { newUser: true } }">
           Sign Up
@@ -17,48 +17,48 @@
     <img id="leftphoto" src="../assets/background5_signin.png" />
 
     <div id="chunk">
-          <p id="sign" v-show="newUser">Sign Up</p>
-          <p id="sign" v-show="!newUser">Log In</p>
-          <form @submit.prevent="letsgo">
-            <div v-if="newUser">
-              <label for="name">Name:</label><br />
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="E.g. John Doe"
-                required
-                v-model="user.name"
-              /><br />
-            </div>
-            <label for="email">Email:</label><br />
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="user@domain.com"
-              required
-              v-model="user.email"
-            /><br />
-
-            <label for="password">Password:</label><br />
-            <input
-              type="text"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              required
-              v-model="user.password"
-            /><br />
-
-            <input type="checkbox" id="checkbox" />
-            <label for="checkbox" id="cblabel">Keep me logged in</label><br />
-
-            <button type="submit" value="Submit" id="submit">
-              Let's go!
-            </button>
-          </form>
+      <p id="sign" v-show="newUser">Sign Up</p>
+      <p id="sign" v-show="!newUser">Log In</p>
+      <form @submit.prevent="letsgo">
+        <div v-if="newUser">
+          <label for="name">Name:</label><br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="E.g. John Doe"
+            required
+            v-model="user.name"
+          /><br />
         </div>
+        <label for="email">Email:</label><br />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="user@domain.com"
+          required
+          v-model="user.email"
+        /><br />
+
+        <label for="password">Password:</label><br />
+        <input
+          type="text"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+          v-model="user.password"
+        /><br />
+
+        <input type="checkbox" id="checkbox" />
+        <label for="checkbox" id="cblabel">Keep me logged in</label><br />
+
+        <button type="submit" value="Submit" id="submit">
+          Let's go!
+        </button>
+      </form>
+    </div>
 
     <img id="rightphoto" src="../assets/background4_signin.png" />
 
@@ -105,27 +105,30 @@
                   user,
                 });
             })
+            .then(() => {
+              this.$router.push({ name: "Home" });
+            })
 
             .catch((error) => {
               alert(error.message);
-            })
-            .then(() => {
-              this.$router.push({ name: "Home" });
             });
+
           event.target.reset();
         } else {
+          //Singing in
           fb.auth()
             .signInWithEmailAndPassword(email, password)
             .then((cred) => {
               alert("Successfully logged into " + cred.user.email);
               // console.log(cred.user.uid);
             })
-            .catch((error) => {
-              alert(error.message);
-            })
             .then(() => {
               this.$router.push({ name: "Home" });
+            })
+            .catch((error) => {
+              alert(error.message);
             });
+
           this.user.email = "";
           this.user.password = "";
           event.target.reset();
@@ -150,114 +153,114 @@
 </script>
 
 <style scoped>
-* {
-  font-weight: lighter;
-  font-size: 13px;
-}
-body {
-  min-width: 100%;
-  width: 100%;
-}
-img {
-  width: 300px;
-  height: 300px;
-}
-ul {
-  position: absolute;
-  right: 10%;
-  top: 2%;
-  list-style-type: None;
-  margin: 0;
-  padding: 0;
-}
+  * {
+    font-weight: lighter;
+    font-size: 13px;
+  }
+  body {
+    min-width: 100%;
+    width: 100%;
+  }
+  img {
+    width: 300px;
+    height: 300px;
+  }
+  ul {
+    position: absolute;
+    right: 10%;
+    top: 2%;
+    list-style-type: None;
+    margin: 0;
+    padding: 0;
+  }
 
-li {
-  font-family: "Lora";
-  display: inline;
-  font-size: 14px;
-  margin-left: 5px;
-  margin-right: 5px;
-  float: right;
-}
+  li {
+    font-family: "Lora";
+    display: inline;
+    font-size: 14px;
+    margin-left: 5px;
+    margin-right: 5px;
+    float: right;
+  }
 
-a {
-  color: black;
-}
+  a {
+    color: black;
+  }
 
-#leftphoto {
-  position: absolute;
-  left: 3%;
-  bottom: 5%;
-  width: 22%;
-  height: auto;
-}
+  #leftphoto {
+    position: absolute;
+    left: 3%;
+    bottom: 5%;
+    width: 22%;
+    height: auto;
+  }
 
-#rightphoto {
-  position: absolute;
-  top: 12%;
-  right: 3%;
-  width: 26%;
-  height: auto;
-}
+  #rightphoto {
+    position: absolute;
+    top: 12%;
+    right: 3%;
+    width: 26%;
+    height: auto;
+  }
 
-#chunk {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+  #chunk {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 
-#sign {
-  font-family: Lora;
-  font-size: 30px;
-  font-weight: bold;
-  margin-bottom: 20px;
-}
+  #sign {
+    font-family: Lora;
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
 
-input {
-  font-family: Roboto;
-  width: 250px;
-  padding-top: 7px;
-  padding-bottom: 7px;
-  padding-left: 10px;
-  padding-right: 10px;
-  margin-top: 3px;
-  margin-bottom: 15px;
-  border-radius: 3px;
-  border-width: thin;
-  border-color: #cccccc;
-  border-style: solid;
-}
+  input {
+    font-family: Roboto;
+    width: 250px;
+    padding-top: 7px;
+    padding-bottom: 7px;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-top: 3px;
+    margin-bottom: 15px;
+    border-radius: 3px;
+    border-width: thin;
+    border-color: #cccccc;
+    border-style: solid;
+  }
 
-::placeholder {
-  color: #cccccc;
-  opacity: 1;
-  font-family: Roboto;
-  font-style: italic;
-}
+  ::placeholder {
+    color: #cccccc;
+    opacity: 1;
+    font-family: Roboto;
+    font-style: italic;
+  }
 
-#checkbox {
-  vertical-align: middle;
-  padding: 5px;
-  margin-top: 5px;
-  margin-bottom: 5px;
-  width: 20px;
-  border-color: #cccccc;
-}
+  #checkbox {
+    vertical-align: middle;
+    padding: 5px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    width: 20px;
+    border-color: #cccccc;
+  }
 
-#cblabel {
-  font-style: italic;
-  font-size: 10px;
-  vertical-align: middle;
-  color: #cccccc;
-}
+  #cblabel {
+    font-style: italic;
+    font-size: 10px;
+    vertical-align: middle;
+    color: #cccccc;
+  }
 
-#submit {
-  width: 270px;
-  padding: 6px;
-  margin-top: 12px;
-  font-family: Lora;
-  border: none;
-  background-color: #bedaae;
-}
+  #submit {
+    width: 270px;
+    padding: 6px;
+    margin-top: 12px;
+    font-family: Lora;
+    border: none;
+    background-color: #bedaae;
+  }
 </style>
