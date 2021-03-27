f89d4f7f2ab37ea02714ecd30f2c290ca4b16f2c
Lin Ru <31382601+l-ru@users.noreply.github.com>
2021-03-27 18:38:48 +0800

    modified LineChart files

--- src/components/Charts/LineChart.vue
+++ src/components/Charts/LineChart.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="chart">
-    <bar-chart></bar-chart>
+    <line-chart></line-chart>
   </div>
 </template>
 
 
 <script>
-import barchart from '../Charts/BarChart.js'
+import linechart from '../Charts/LineChart.js'
 
 export default {
   components: {
-    'bar-chart': barchart,
+    'line-chart': linechart,
   }
 };
 </script>
