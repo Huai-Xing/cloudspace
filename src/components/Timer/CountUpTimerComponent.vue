dc2342a922e3ebbd35878cb8ea7a9ecb20f8cbb3
HX <huaixing1234@gmail.com>
2021-03-29 20:51:13 +0800

    Edited styling of notes and tooltip for timer

--- src/components/Timer/CountUpTimerComponent.vue
+++ src/components/Timer/CountUpTimerComponent.vue
@@ -1,42 +1,54 @@
 <template>
-<div>
-  <br>
-  <h2 id="timer-title"> {{title}} </h2>
+  <div>
+    <br />
+    <h2 id="timer-title">{{ title }}</h2>
 
-  <div class="timer-container">
-    <svg class="timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <g class="timer__circle">
-        <circle class="timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-        <circle class="timer__inner-circle" cx="50" cy="50" r="35.5"></circle>
-        <path id="test"
-          :stroke-dasharray="circleDasharray"
-          class="timer__path-remaining"
-          :class="remainingPathColor"
-          d="
+    <div class="timer-container">
+      <svg
+        class="timer__svg"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g class="timer__circle">
+          <circle class="timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+          <circle class="timer__inner-circle" cx="50" cy="50" r="35.5"></circle>
+          <path
+            id="test"
+            :stroke-dasharray="circleDasharray"
+            class="timer__path-remaining"
+            :class="remainingPathColor"
+            d="
             M 50, 50
             m -45, 0
             a 45,45 0 1,1 90,0
             a 45,45 0 1,1 -90,0
-          ">
-        </path>
-      </g>
-    </svg>
+          "
+          ></path>
+        </g>
+      </svg>
 
-    <!-- to show the remaining time in the timer -->
-    <span class="timer__label"> {{ formattedTimeUsed }} <br> elapsed </span>
+      <!-- to show the remaining time in the timer -->
+      <span class="timer__label">
+        {{ formattedTimeUsed }} <br />
+        elapsed
+      </span>
+    </div>
+
+    <br /><br />
+
+    <div class="buttons">
+      <!-- Timer completes -->
+      <button id="done" class="timerControlledBtns" @click="doneTimer()">
+        Resume task
+      </button>
+      <p id="note">
+        Note: Recommended break time is
+        <u><b>{{ breakTimeAllowedDisplay }}.</b></u>
+        <br />Penalty will be imposed on coins earned for breaks exceeding
+        recommended limit!
+      </p>
+    </div>
   </div>
-
-  <br><br>
-
-  <div class="buttons">
-    <!-- Timer completes -->
-    <button id="done" class="timerControlledBtns" @click="doneTimer()">
-        Go back to task
-    </button>
-    <p id="note">Note: Recommended break time is {{breakTimeAllowedDisplay}}.<br>Penalty will be imposed on coins earned for breaks exceeding recommended limit!</p>
-  </div>
-
-</div>
 </template>
 
 
@@ -53,14 +65,14 @@ const COLOR_CODES = {
 //const this.allowedTime= 900; // dummy timing value start from 0 for testing at the moment
 
 export default {
-    props:{
-      breakTime: Number,
-      breakTimePassed: Number,
-      breakTimeAllowed: Number,
-    },
+  props: {
+    breakTime: Number,
+    breakTimePassed: Number,
+    breakTimeAllowed: Number,
+  },
   data() {
     return {
-      timePassed: 0, /* To store the amount of time (in sec) that has passed */
+      timePassed: 0 /* To store the amount of time (in sec) that has passed */,
       timerInterval: null,
       totalTimePassed: 0,
       title: "Break time!",
@@ -106,9 +118,10 @@ export default {
     },
 
     // Update the dasharray value as time passes, starting with 283
-    circleDasharray() { // To animate the ring arc length of the remaining time line with the 'stroke-dasharray' in <path>
+    circleDasharray() {
+      // To animate the ring arc length of the remaining time line with the 'stroke-dasharray' in <path>
       //return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)} 283`;
-      return `${((1- this.timeFraction) * FULL_DASH_ARRAY).toFixed(0)} 283`;
+      return `${((1 - this.timeFraction) * FULL_DASH_ARRAY).toFixed(0)} 283`;
     },
 
     timeFraction() {
@@ -121,8 +134,7 @@ export default {
     remainingPathColor() {
       const { info } = COLOR_CODES;
       return info.color;
-    }
-
+    },
   },
 
   watch: {
@@ -130,10 +142,11 @@ export default {
       if (newValue === 0) {
         this.onTimesUp();
       }
-    }
+    },
   },
 
-  mounted() { // to start the timer immediately when the component gets mounted
+  mounted() {
+    // to start the timer immediately when the component gets mounted
     this.totalTimePassed = this.breakTime - this.breakTimePassed;
     this.timePassed = this.breakTimePassed;
     this.allowedTime = this.breakTimeAllowed;
@@ -141,41 +154,43 @@ export default {
       this.breakTimeAllowedDisplay = String(this.breakTimeAllowed) + " seconds";
     } else {
       if (this.breakTimeAllowed % 60 == 0) {
-        this.breakTimeAllowedDisplay = String(this.breakTimeAllowed / 60) + " minutes";
+        this.breakTimeAllowedDisplay =
+          String(this.breakTimeAllowed / 60) + " minutes";
       } else {
         var seconds = this.breakTimeAllowed % 60;
         var minutes = Math.floor(this.breakTimeAllowed / 60);
-        this.breakTimeAllowedDisplay = String(minutes) + " minutes & " + String(seconds) + " seconds";
+        this.breakTimeAllowedDisplay =
+          String(minutes) + " minutes & " + String(seconds) + " seconds";
       }
     }
     this.startTimer();
   },
 
   methods: {
-    resetTIMEPASSED: function() {
+    resetTIMEPASSED: function () {
       this.timePassed = 0;
       this.totalTimePassed = this.allowedTime + this.totalTimePassed;
     },
-    onTimesUp: function() {
+    onTimesUp: function () {
       clearInterval(this.timerInterval);
     },
 
     // Fn that increase the value of timePassed by 1unit per sec and recompute the timeUsed value
-    startTimer: function() {
+    startTimer: function () {
       // increment the timePassed value every sec
       this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
     },
 
-    doneTimer: function() {
+    doneTimer: function () {
       var timeGain = 0;
       if (this.totalTimePassed == 0) {
         timeGain = this.timePassed;
       } else {
         timeGain = this.totalTimePassed + this.timePassed;
       }
-      this.$emit('switch', false, timeGain, this.timePassed);
+      this.$emit("switch", false, timeGain, this.timePassed);
     },
-  }
+  },
 };
 </script>
 
@@ -225,7 +240,7 @@ button {
 }
 
 .timer__inner-circle {
-  fill: #F27D84;
+  fill: #f27d84;
   opacity: 0.47;
 }
 
@@ -240,12 +255,12 @@ button {
 
   transition: 1s linear all; /* One second aligns with the speed of the countdown timer */
   fill-rule: nonzero;
-  stroke: currentColor;  /* Allows the ring to change color when the color value updates */
+  stroke: currentColor; /* Allows the ring to change color when the color value updates */
   opacity: 1;
 }
 
 .timer__path-remaining.red {
-  color: #F27D84;
+  color: #f27d84;
 }
 
 /* Styling to show the timing value inside the circle */
