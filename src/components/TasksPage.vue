6ed044544bd42fc9fee1557803436a90257737a0
leannetan <65459434+leannetan@users.noreply.github.com>
2021-04-01 18:10:10 +0800

    Fixed calendar to task refresh

--- src/components/TasksPage.vue
+++ src/components/TasksPage.vue
@@ -104,283 +104,279 @@
 </template>
 
 <script>
-  import fb from "../firebase";
-  import EditTaskForm from "./EditTaskForm.vue";
-  import MainNavigation from "./MainNavigation.vue";
-  import NewTaskForm from "./NewTaskForm.vue";
-  import dayjs from "dayjs";
-  import NewDeadlineForm from "./NewDeadlineForm.vue";
-  import EditDeadlineForm from "./EditDeadlineForm.vue";
+import fb from "../firebase";
+import EditTaskForm from "./EditTaskForm.vue";
+import MainNavigation from "./MainNavigation.vue";
+import NewTaskForm from "./NewTaskForm.vue";
+import dayjs from "dayjs";
+import NewDeadlineForm from "./NewDeadlineForm.vue";
+import EditDeadlineForm from "./EditDeadlineForm.vue";
 
-  export default {
-    data() {
-      return {
-        date: dayjs(),
-        user: fb.auth().currentUser.uid,
-        tasks: [],
-        deadlines: [],
-        isToday: true,
-      };
+export default {
+  data() {
+    return {
+      date: dayjs(),
+      user: fb.auth().currentUser.uid,
+      tasks: [],
+      deadlines: [],
+      isToday: true,
+    };
+  },
+  //Register Locally
+  components: {
+    appNav: MainNavigation,
+    NewTaskForm,
+    EditTaskForm,
+    NewDeadlineForm,
+    EditDeadlineForm,
+  },
+  methods: {
+    //Checking which tasks to display
+    checkTaskDate: function (task) {
+      console.log(task[1].date.toDate());
+      var date = this.date.get("date");
+      var month = this.date.get("month");
+      var year = this.date.get("year");
+      let start = new Date(year, month, date, 0, 0, 0);
+      let end = new Date(year, month, date, 23, 59, 59);
+      if (task[1].date.toDate() >= start && task[1].date.toDate() <= end) {
+        return true;
+      } else {
+        false;
+      }
     },
-    //Register Locally
-    components: {
-      appNav: MainNavigation,
-      NewTaskForm,
-      EditTaskForm,
-      NewDeadlineForm,
-      EditDeadlineForm,
+    checkDeadlineDate: function (deadline) {
+      let duedate = new Date(deadline[1].datedue);
+      var month = duedate.getMonth();
+      var date = duedate.getDate();
+      var year = duedate.getFullYear();
+      var end = new Date(
+        year,
+        month,
+        date,
+        deadline[1].timedue.hh,
+        deadline[1].timedue.mm,
+        59
+      );
+      var start = new Date(
+        year,
+        month,
+        date - deadline[1].showInAdvance,
+        0,
+        0,
+        0
+      );
+      console.log(start);
+      console.log(end);
+
+      if (this.date >= start && this.date <= end) {
+        return true;
+      } else {
+        return false;
+      }
     },
-    methods: {
-      //Checking which tasks to display
-      checkTaskDate: function(task) {
-        console.log(task[1].date.toDate());
-        var date = this.date.get("date");
-        var month = this.date.get("month");
-        var year = this.date.get("year");
-        let start = new Date(year, month, date, 0, 0, 0);
-        let end = new Date(year, month, date, 23, 59, 59);
-        if (task[1].date.toDate() >= start && task[1].date.toDate() <= end) {
-          return true;
-        } else {
-          false;
-        }
-      },
-      checkDeadlineDate: function(deadline) {
-        let duedate = new Date(deadline[1].datedue);
-        var month = duedate.getMonth();
-        var date = duedate.getDate();
-        var year = duedate.getFullYear();
-        var end = new Date(
-          year,
-          month,
-          date,
-          deadline[1].timedue.hh,
-          deadline[1].timedue.mm,
-          59
-        );
-        var start = new Date(
-          year,
-          month,
-          date - deadline[1].showInAdvance,
-          0,
-          0,
-          0
-        );
-        console.log(start);
-        console.log(end);
-
-        if (this.date >= start && this.date <= end) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-      //Fetching all user's tasks
-      fetchTasks: function() {
-        fb.firestore()
-          .collection("tasks")
-          .doc(this.user)
-          .collection("tasksList")
-          .get()
-          .then((snapshot) => {
-            snapshot.forEach((doc) => {
-              this.tasks.push([doc.id, doc.data()]);
-            });
+    //Fetching all user's tasks
+    fetchTasks: function () {
+      fb.firestore()
+        .collection("tasks")
+        .doc(this.user)
+        .collection("tasksList")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.tasks.push([doc.id, doc.data()]);
           });
-      },
-      //Fetching user's deadlines
-      fetchDeadLines: function() {
-        fb.firestore()
-          .collection("tasks")
-          .doc(this.user)
-          .collection("deadlinesList")
-          .get()
-          .then((snapshot) => {
-            snapshot.forEach((doc) => {
-              this.deadlines.push([doc.id, doc.data()]);
-            });
-          });
-      },
-      change: function(x) {
-        if (x == 0) {
-          this.date = this.date.subtract(1, "day");
-        } else if (x == 1) {
-          this.date = this.date.add(1, "day");
-        } else {
-          this.date = dayjs();
-        }
-        this.isToday = this.date.isSame(dayjs(), "day");
-      },
-
-      deleteTask: function(event) {
-        let doc_id = event.target.getAttribute("idname");
-        fb.firestore()
-          .collection("tasks")
-          .doc(this.user)
-          .collection("tasksList")
-          .doc(doc_id)
-          .delete()
-          .then(() => {
-            location.reload();
-          });
-      },
-      deleteDeadline: function(event) {
-        let doc_id = event.target.getAttribute("idname");
-        fb.firestore()
-          .collection("tasks")
-          .doc(this.user)
-          .collection("deadlinesList")
-          .doc(doc_id)
-          .delete()
-          .then(() => {
-            location.reload();
-          });
-      },
-      startTask: function(event) {
-        let doc_id = event.target.getAttribute("idname");
-
-        fb.firestore()
-          .collection("tasks")
-          .doc(this.user)
-          .collection("tasksList")
-          .doc(doc_id)
-          .get()
-          .then((doc) => {
-            let totalseconds =
-              doc.data().duration.hh * 3600 + doc.data().duration.mm * 60;
-            console.log(totalseconds);
-            let title = doc.data().category + " - " + doc.data().title;
-            this.$router.push({
-              name: "Timer",
-              params: {
-                taskId: doc_id,
-                timeForTask: totalseconds,
-                taskTitle: title,
-              },
-            });
-          });
-      },
+        });
     },
-    created() {
-      this.fetchTasks();
-      this.fetchDeadLines();
-      var passedDate = this.$route.params;
-      if (Object.keys(passedDate).length != 0) {
-        // if task page came from calendar
-        this.date = passedDate.date;
-        console.log("test");
-        console.log(passedDate);
-        this.isToday = this.date.isSame(dayjs(), "day");
+    //Fetching user's deadlines
+    fetchDeadLines: function () {
+      fb.firestore()
+        .collection("tasks")
+        .doc(this.user)
+        .collection("deadlinesList")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.deadlines.push([doc.id, doc.data()]);
+          });
+        });
+    },
+    change: function (x) {
+      if (x == 0) {
+        this.date = this.date.subtract(1, "day");
+      } else if (x == 1) {
+        this.date = this.date.add(1, "day");
       } else {
         this.date = dayjs();
       }
+      this.$router.push({
+        name: "Tasks",
+        params: { date: this.date.format() },
+      });
+      this.isToday = this.date.isSame(dayjs(), "day");
     },
-  };
+
+    deleteTask: function (event) {
+      let doc_id = event.target.getAttribute("idname");
+      fb.firestore()
+        .collection("tasks")
+        .doc(this.user)
+        .collection("tasksList")
+        .doc(doc_id)
+        .delete()
+        .then(() => {
+          location.reload();
+        });
+    },
+    deleteDeadline: function (event) {
+      let doc_id = event.target.getAttribute("idname");
+      fb.firestore()
+        .collection("tasks")
+        .doc(this.user)
+        .collection("deadlinesList")
+        .doc(doc_id)
+        .delete()
+        .then(() => {
+          location.reload();
+        });
+    },
+    startTask: function (event) {
+      let doc_id = event.target.getAttribute("idname");
+
+      fb.firestore()
+        .collection("tasks")
+        .doc(this.user)
+        .collection("tasksList")
+        .doc(doc_id)
+        .get()
+        .then((doc) => {
+          let totalseconds =
+            doc.data().duration.hh * 3600 + doc.data().duration.mm * 60;
+          console.log(totalseconds);
+          let title = doc.data().category + " - " + doc.data().title;
+          this.$router.push({
+            name: "Timer",
+            params: {
+              taskId: doc_id,
+              timeForTask: totalseconds,
+              taskTitle: title,
+            },
+          });
+        });
+    },
+  },
+  created() {
+    this.fetchTasks();
+    this.fetchDeadLines();
+    this.date = dayjs(this.$route.params.date);
+    this.isToday = this.date.isSame(dayjs(), "day");
+  },
+};
 </script>
 
 <style scoped>
-  .task {
-    margin-left: 240px;
-  }
-  h2 {
-    font-family: Lora;
-    font-size: 20px;
-    color: black;
-    display: inline-block;
-    text-align: center;
-    width: 180px;
-    margin-top: -100px;
-  }
-  .arrow {
-    border: solid black;
-    border-width: 0 3px 3px 0;
-    display: inline-block;
-    padding: 2px;
-    cursor: pointer;
-  }
-  .right {
-    transform: rotate(-45deg);
-    -webkit-transform: rotate(-45deg);
-    width: 2px;
-    height: 2px;
-  }
-  .left {
-    transform: rotate(135deg);
-    -webkit-transform: rotate(135deg);
-    width: 2px;
-    height: 2px;
-  }
-  .right:hover,
-  .left:hover {
-    filter: opacity(0.6);
-  }
-  span {
-    align-items: center;
-    margin-left: 20px;
-    text-decoration: underline;
-    cursor: pointer;
-  }
-  .backToToday {
-    font-family: lora;
-    color: #4d4d4d;
-  }
-  .deadlines {
-    min-height: 150px;
-    max-height: 150px;
-  }
-  .sublabel {
-    font-family: lora;
-    font-size: 16px;
-    font-weight: 600;
-    display: inline-block;
-  }
-  .addTask {
-    float: right;
-    margin-right: 6%;
-  }
-  .line {
-    height: 2px;
-    width: 96%;
-    float: left;
-    background-color: #607c868d;
-    border: none;
-  }
-  .label-container {
-    text-align: center;
-    color: #34b2c5;
-    display: grid;
-    min-width: 96%;
-    padding: 20px;
-    grid-template-columns: 120px repeat(3, 1fr) 400px;
-  }
-  .taskLabel {
-    font-family: montserrat;
-    text-decoration: none;
-    font-size: 15px;
-    font-weight: 600;
-    text-transform: uppercase;
-    text-decoration: none;
-  }
-  .task-container {
-    text-align: center;
-    display: grid;
-    min-width: 96%;
-    padding: 10px;
-    grid-template-columns: 120px repeat(3, 1fr) 400px;
-  }
-  .taskText {
-    font-family: Lora;
-    text-decoration: none;
-    font-size: 12px;
-    padding-top: 10px;
-  }
-  img {
-    height: 28px;
-    width: auto;
-    margin: 2px;
-    text-align: center;
-  }
-  .editTaskIcon {
-    display: inline-block;
-  }
+.task {
+  margin-left: 240px;
+}
+h2 {
+  font-family: Lora;
+  font-size: 20px;
+  color: black;
+  display: inline-block;
+  text-align: center;
+  width: 180px;
+  margin-top: -100px;
+}
+.arrow {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 2px;
+  cursor: pointer;
+}
+.right {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+  width: 2px;
+  height: 2px;
+}
+.left {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+  width: 2px;
+  height: 2px;
+}
+.right:hover,
+.left:hover {
+  filter: opacity(0.6);
+}
+span {
+  align-items: center;
+  margin-left: 20px;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.backToToday {
+  font-family: lora;
+  color: #4d4d4d;
+}
+.deadlines {
+  min-height: 150px;
+  max-height: 150px;
+}
+.sublabel {
+  font-family: lora;
+  font-size: 16px;
+  font-weight: 600;
+  display: inline-block;
+}
+.addTask {
+  float: right;
+  margin-right: 6%;
+}
+.line {
+  height: 2px;
+  width: 96%;
+  float: left;
+  background-color: #607c868d;
+  border: none;
+}
+.label-container {
+  text-align: center;
+  color: #34b2c5;
+  display: grid;
+  min-width: 96%;
+  padding: 20px;
+  grid-template-columns: 120px repeat(3, 1fr) 400px;
+}
+.taskLabel {
+  font-family: montserrat;
+  text-decoration: none;
+  font-size: 15px;
+  font-weight: 600;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+.task-container {
+  text-align: center;
+  display: grid;
+  min-width: 96%;
+  padding: 10px;
+  grid-template-columns: 120px repeat(3, 1fr) 400px;
+}
+.taskText {
+  font-family: Lora;
+  text-decoration: none;
+  font-size: 12px;
+  padding-top: 10px;
+}
+img {
+  height: 28px;
+  width: auto;
+  margin: 2px;
+  text-align: center;
+}
+.editTaskIcon {
+  display: inline-block;
+}
 </style>
